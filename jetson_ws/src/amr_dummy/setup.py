828cc9c5c2a66f3049226a0082d8d11c61815ffd
Sung-Won-AI <sbae7071@gmail.com>
2026-08-20 18:14:48 +0900

    Create setup.py

---
+++ jetson_ws/src/amr_dummy/setup.py
@@ -0,0 +1,24 @@
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = "amr_dummy"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        ("share/" + package_name + "/config", glob("config/*.yaml")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    entry_points={
+        "console_scripts": [
+            "dummy_sensor_node = amr_dummy.dummy_sensor_node:main",
+            "dummy_mcu_node = amr_dummy.dummy_mcu_node:main",
+        ]
+    },
+)
